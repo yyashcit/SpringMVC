396943731ec5532b9f7748552a99261c4d483fa2
nitesh0402 <niteshverma0204@gmail.com>
2022-12-31 20:56:55 +0530

    "new method added"

--- SpringMVCFormValidation/src/com/infotech/service/StudentService.java
+++ SpringMVCFormValidation/src/com/infotech/service/StudentService.java
@@ -7,3 +7,9 @@ public interface StudentService {
 	public abstract boolean registerStudent(Student student);
 
 }
+
+public interface myService {
+	public abstract Student validateStudentCredential(String email,	String password);
+	public abstract boolean registerStudent(Student student);
+
+}
